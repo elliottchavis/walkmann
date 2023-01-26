00ad4a13a80da12dae19674ad4de9bcf5cfe20cc
zee tredded <zeetredded@zees-MacBook-Air.local>
2023-01-26 23:23:33 +0700

    actually didn't use stackviewsgit add .

--- walkmann/ViewController.swift
+++ walkmann/ViewController.swift
@@ -9,10 +9,28 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    // MARK: - Properties
+    
+    @IBOutlet weak var activityBoardLabel: UILabel!
+    @IBOutlet weak var moveDataLabel: UILabel!
+    @IBOutlet weak var stepsDataLabel: UILabel!
+    @IBOutlet weak var distanceDataLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         // Do any additional setup after loading the view.
+        
+        configureUI()
+        
+    }
+    
+    // MARK: - Functions
+    
+    func configureUI() {
         view.backgroundColor = .black
+        activityBoardLabel?.layer.masksToBounds = true
+        activityBoardLabel?.layer.cornerRadius = 15
     }
 
 
