e94ab74e93fd450af1ff78787ec226d30a5de621
zee tredded <zeetredded@zees-MacBook-Air.local>
2023-01-27 02:16:28 +0700

    added cal and mile count

--- walkmann/ViewController.swift
+++ walkmann/ViewController.swift
@@ -42,7 +42,7 @@ class ViewController: UIViewController {
     func healthAuth() {
         
         // Access Step Count
-        let healthKitTypes: Set = [ HKObjectType.quantityType(forIdentifier: HKQuantityTypeIdentifier.stepCount)! ]
+        let healthKitTypes: Set = [ HKObjectType.quantityType(forIdentifier: HKQuantityTypeIdentifier.stepCount)!, HKObjectType.quantityType(forIdentifier: HKQuantityTypeIdentifier.distanceWalkingRunning)! , HKObjectType.quantityType(forIdentifier: HKQuantityTypeIdentifier.activeEnergyBurned)!]
         
         // Check for Authorization
         healthStore.requestAuthorization(toShare: healthKitTypes, read: healthKitTypes) { (bool, error) in
@@ -52,6 +52,22 @@ class ViewController: UIViewController {
                     DispatchQueue.main.async {
                         let stepCount = String(Int(result))
                         self.stepsDataLabel.text = String(stepCount)
+                    }
+                }
+                
+                self.getDistance { (result) in
+                    DispatchQueue.main.async {
+                        let finalResult = result * 0.000189394
+                        let distanceCount = String(format: "%.2f", finalResult)
+                        print(distanceCount)
+                        self.distanceDataLabel.text = "\(distanceCount) miles"
+                    }
+                }
+                
+                self.getCalories { (result) in
+                    DispatchQueue.main.async {
+                        let caloricCount = String(Int(result))
+                        self.moveDataLabel.text = "\(caloricCount)/300cal"
                     }
                 }
             }
@@ -78,7 +94,7 @@ class ViewController: UIViewController {
                 result!.enumerateStatistics(from: startOfDay, to: now) { statistics, _ in
 
                 if let sum = statistics.sumQuantity() {
-                    // Get steps (they are of double type)
+                    // Get distance (they are of double type)
                     resultCount = sum.doubleValue(for: HKUnit.count())
                 } // end if
 
@@ -105,6 +121,111 @@ class ViewController: UIViewController {
         healthStore.execute(query)
 
     }
+    
+    
+    
+    func getDistance(completion: @escaping (Double) -> Void) {
+        let type = HKQuantityType.quantityType(forIdentifier: .distanceWalkingRunning)!
+            
+        let now = Date()
+        let startOfDay = Calendar.current.startOfDay(for: now)
+        var interval = DateComponents()
+        interval.day = 1
+        
+        let query = HKStatisticsCollectionQuery(quantityType: type,
+                                               quantitySamplePredicate: nil,
+                                               options: [.cumulativeSum],
+                                               anchorDate: startOfDay,
+                                               intervalComponents: interval)
+        
+        query.initialResultsHandler = { _, result, error in
+                var resultCount = 0.0
+                result!.enumerateStatistics(from: startOfDay, to: now) { statistics, _ in
+
+                if let sum = statistics.sumQuantity() {
+                    // Get steps (they are of double type)
+                    resultCount = sum.doubleValue(for: HKUnit.foot())
+                } // end if
+
+                // Return
+                DispatchQueue.main.async {
+                    completion(resultCount)
+                }
+            }
+        }
+        
+        query.statisticsUpdateHandler = {
+            query, statistics, statisticsCollection, error in
+
+            // If new statistics are available
+            if let sum = statistics?.sumQuantity() {
+                let resultCount = sum.doubleValue(for: HKUnit.foot())
+                // Return
+                DispatchQueue.main.async {
+                    completion(resultCount)
+                }
+            } // end if
+        }
+        
+        healthStore.execute(query)
+
+    }
+    
+    
+    
+    
+    func getCalories(completion: @escaping (Double) -> Void) {
+        let type = HKQuantityType.quantityType(forIdentifier: .activeEnergyBurned)!
+            
+        let now = Date()
+        let startOfDay = Calendar.current.startOfDay(for: now)
+        var interval = DateComponents()
+        interval.day = 1
+        
+        let query = HKStatisticsCollectionQuery(quantityType: type,
+                                               quantitySamplePredicate: nil,
+                                               options: [.cumulativeSum],
+                                               anchorDate: startOfDay,
+                                               intervalComponents: interval)
+        
+        query.initialResultsHandler = { _, result, error in
+                var resultCount = 0.0
+                result!.enumerateStatistics(from: startOfDay, to: now) { statistics, _ in
+
+                if let sum = statistics.sumQuantity() {
+                    // Get steps (they are of double type)
+                    resultCount = sum.doubleValue(for: HKUnit.kilocalorie())
+                } // end if
+
+                // Return
+                DispatchQueue.main.async {
+                    completion(resultCount)
+                }
+            }
+        }
+        
+        query.statisticsUpdateHandler = {
+            query, statistics, statisticsCollection, error in
+
+            // If new statistics are available
+            if let sum = statistics?.sumQuantity() {
+                let resultCount = sum.doubleValue(for: HKUnit.kilocalorie())
+                // Return
+                DispatchQueue.main.async {
+                    completion(resultCount)
+                }
+            } // end if
+        }
+        
+        healthStore.execute(query)
+
+    }
+    
+    
+    
+    
+    
+    
     
 
 
