5444da99d7e45e68bc4cec9424bfc5f5ff37f3c5
zee tredded <zeetredded@zees-MacBook-Air.local>
2023-02-03 23:39:43 +0700

    added UIStepper counter

--- walkmann/GoalController.swift
+++ walkmann/GoalController.swift
@@ -9,10 +9,22 @@ import UIKit
 
 class GoalController: UIViewController {
     
+    @IBOutlet weak var countLabel: UILabel!
+    @IBOutlet weak var stepper: UIStepper!
     
     // MARK: - Lifecycle
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        stepper.backgroundColor = .systemOrange
+        stepper.value = 100
+        stepper.minimumValue = 10
     }
+    
+    @IBAction func stepperPressed(_ sender: UIStepper) {
+        
+        countLabel.text = Int(sender.value).description
+    }
+    
+    
 }
